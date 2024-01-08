20e9ee48174f140764b2880bed9cbe54ca9653fa
MelikKeskiner <melikkeskiner0@gmail.com>
2024-01-07 21:36:21 -0800

    my test is done

--- src/MelikTest.java
+++ src/MelikTest.java
@@ -5,39 +5,110 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.DriverClass;
+import utilities.MyMethods;
 
 import java.time.Duration;
 
 public class MelikTest extends DriverClass {
     @Test
-    public void test() {
+    public void test(){
+        driver.get("https://www.lidl.com/login");
+
+        WebElement joinMyLidl = driver.findElement(By.linkText("join myLidl"));
+        joinMyLidl.click();
+
+        WebElement firstName = driver.findElement(By.name("firstName"));
+        firstName.sendKeys("Melik");
+
+        WebElement lastName = driver.findElement(By.name("lastName"));
+        lastName.sendKeys("Keskiner");
+
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("melikkeskiner0@gmail.com");
+
+        WebElement confirm_email = driver.findElement(By.name("confirm_email"));
+        confirm_email.sendKeys("melikkeskiner0@gmail.com");
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("PassW0rd");
+
+        WebElement phone = driver.findElement(By.name("phone"));
+        phone.sendKeys("(111) 123-4321");
+
+        WebElement zip = driver.findElement(By.name("zip"));
+        zip.sendKeys("11229");
+
+        WebElement birthday = driver.findElement(By.name("birthday"));
+        birthday.sendKeys("01/01/1990");
+
+        WebElement cokieClose = driver.findElement(By.xpath("//button[@aria-label=\"Close\"]"));
+        cokieClose.click();
+
+        WebElement joinMyLidlSubmit = driver.findElement(By.xpath("//button[text()=\"join myLidl\"]"));
+        joinMyLidlSubmit.click();
+
+        MyMethods.myWait(3);
+        driver.quit();
+    }
+
+    @Test
+    public void test1() {
+        MyMethods.myWait(2);
         driver.get("https://www.lidl.com/");
 
-        WebElement cancel = driver.findElement(By.xpath("//img[@alt=\"close-white description\"]"));
-        cancel.click();
+        WebElement bannerClose = driver.findElement(By.xpath("//button[@aria-label=\"okay\"]"));
+        bannerClose.click();
+        MyMethods.myWait(1);
+
+        WebElement cokieClose = driver.findElement(By.xpath("//button[@aria-label=\"Close\"]"));
+        cokieClose.click();
+
+        MyMethods.myWait(2);
+
+        WebElement signInButton = driver.findElement(By.linkText("sign in"));
+        signInButton.click();
+
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("melikkeskiner0@gmail.com");
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("PassW0rd");
 
         WebElement myAccount =
-                driver.findElement(By.xpath("//span[@class=\"profile-dropdown-button-label \"]"));
+                driver.findElement(By.cssSelector("button[data-test=\"signInButton\"]"));
         myAccount.click();
-        WebElement myAcc = driver.findElement(By.xpath("//div[@class=\"name\"]"));
-        myAcc.click();
+
+        WebElement account =
+                driver.findElement(By.xpath("//span[@class=\"profile-dropdown-button-label \"]"));
+        account.click();
+
+        WebElement name = driver.findElement(By.xpath("//div[@class=\"name\"]"));
+        name.click();
 
         Assert.assertTrue(driver.getCurrentUrl().contains("profile"));
+        MyMethods.myWait(2);
 
-        WebElement firstName = driver.findElement(By.id("input19"));
-        firstName.sendKeys("melikk");
-        WebElement lastName = driver.findElement(By.id("input20"));
-        lastName.sendKeys("Keskinerr");
+        WebElement firstName = driver.findElement(By.name("firstName"));
+        firstName.sendKeys("k");
+        WebElement lastName = driver.findElement(By.name("lastName"));
+        lastName.sendKeys("r");
 
         WebElement save = driver.findElement(By.cssSelector("button[type=\"submit\"]"));
         save.click();
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.textToBe(By.cssSelector("button[type=\"submit\"]"), "profile updated successfully"));
+        MyMethods.myWait(2);
+
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//        wait.until(ExpectedConditions.textToBe(By.cssSelector("button[type=\"submit\"]"), "profile updated successfully"));
+
+        WebElement chec = driver.findElement(By.cssSelector("button[type=\"submit\"]"));
+        chec.getText().contains("profile updated successfully");
 
         WebElement check = driver.findElement(By.xpath("//h3[@class=\"title-3\"]"));
 
-        Assert.assertTrue(check.getText().equals("melikk keskinerr"));
+        Assert.assertTrue(check.getText().equals("Melikk Keskinerr"));
+
+        waitAndQuit();
 
     }
 }
