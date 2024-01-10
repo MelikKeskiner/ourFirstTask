9f609d5724711c5c1b27ca59686be3408d1428bd
jibril <jibril20k@gmail.com>
2024-01-10 17:37:11 -0500

    Jabril Task1

--- src/Task1/Task1.java
+++ src/Task1/Task1.java
@@ -5,18 +5,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+
 
 public class Task1 extends utilities.DriverClass {
      //  I should be able to click on the "Sign In" button on the homepage.
     // By clicking on the "Join MyLidl" button from the "Don't have an account?" link, I should be redirected to the registration page.
     @Test
     public void signIn(){
+
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.lidl.com/");
 
         WebElement signIn = driver.findElement(By.linkText("sign in"));
         signIn.click();
+
         utilities.MyMethods.myWait(5);
 
 
@@ -25,13 +27,12 @@ public class Task1 extends utilities.DriverClass {
 
     @Test
     public void join(){
+
         driver.get("https://www.lidl.com/login");
-
-
-
 
         WebElement join_myLidl = driver.findElement(By.linkText("join myLidl"));
         join_myLidl.click();
+
         utilities.MyMethods.myWait(5);
     }
 
